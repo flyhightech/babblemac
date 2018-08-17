238f3f8a28f74337330f59f4a358268825159928
Bernard Huff <bernard.huff008@gmail.com>
2018-08-16 20:35:49 -0500

    Initial Commit

---
+++ babblemac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  babblemac
+//
+//  Created by Bernard Huff on 8/16/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ babblemac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  babblemac
+//
+//  Created by Bernard Huff on 8/16/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
